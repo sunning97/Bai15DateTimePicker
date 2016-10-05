c4ab298ac56c26993d8fc6e2ef14f98ba6f17fd9
Thangcoder <trangchongchengit@gmail.com>
2016-10-05 23:42:28 +0700

    Initial commit

---
+++ app/src/main/java/com/thangcoder/bai15datetimepicker/MainActivity.java
@@ -0,0 +1,47 @@
+package com.thangcoder.bai15datetimepicker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DatePicker datePicker;
+    private TimePicker timePicker;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        datePicker = (DatePicker) findViewById(R.id.datePicker);
+        timePicker = (TimePicker) findViewById(R.id.timePicker);
+        initDatePicker();
+        initTimePicker();
+    }
+
+    public void initDatePicker(){
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(calendar.YEAR);
+        final int month = calendar.get(calendar.MONTH);
+        final int day = calendar.get(calendar.DAY_OF_MONTH);
+        datePicker.init(year, month, day, new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                Toast.makeText(MainActivity.this, dayOfMonth+"-"+ (monthOfYear+1) +"-"+year, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    public void initTimePicker(){
+        timePicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+            @Override
+            public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
+                Toast.makeText(MainActivity.this, hourOfDay+":"+minute, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
